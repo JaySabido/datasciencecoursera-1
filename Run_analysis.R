275ec182e8ce1cb0e03ef3bb1dba3afc3e7b1f3e
Leonardo Marques Maciel Silva <leonardorg15@gmail.com>
2017-05-20 13:58:02 -0300

    File format
    I've just formatted this file :)

--- Run_analysis.R
+++ Run_analysis.R
@@ -1,4 +1,4 @@
-#04/05/2017 Leonardo Marques
+#20/05/2017 Leonardo Marques
 #Download and unzip file
 fname <- "UCIHARDS.zip"
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
@@ -14,7 +14,6 @@ x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 sbj_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 
-
 # 2 - Extracts only the measurements on the mean and standard deviation for each measurement.
 features <- read.table("UCI HAR Dataset/features.txt")
 calc <- grep(".*mean.*|.*std.*", features[, 2])
@@ -27,6 +26,7 @@ train <- cbind(sbj_train,y_train,x_train_final)
 
 # 3 - Uses descriptive activity names to name the activities in the data set
 act_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+
 # 4 - Appropriately label the data set with descriptive variable names
 all <- rbind(train,test)
 calc.names <- gsub('-','',calc.names)
@@ -36,10 +36,6 @@ colnames(all) <- c("subject", "activity", calc.names)
 # 5 From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 all$activity <- factor(all$activity, levels = act_labels[,1], labels = act_labels[,2])
 all$subject <- as.factor(all$subject)
-
 all.melted <- melt(all, id = c("subject", "activity"))
 all.mean <- dcast(all.melted, subject + activity ~ variable, mean)
-
 write.table(all.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
-
-
